57b1e7f23133d24638414224a882070859b4674d
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-27 16:13:39 -0300

    Atualização de comentários

--- DumpComentado/DumpDiferenteComentado.php
+++ DumpComentado/DumpDiferenteComentado.php
@@ -1,80 +1,81 @@
 <?php
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Criado e atualizado por José Augusto Chaves Izabel
+
+// 12/02/2020
+
+// Uso do var_dump com o "!=" e "!==".
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Criado e atualizado por José Augusto Chaves Izabel
+// Criação de variável.
 
-//12/02/2020
+$FirstValue = 12.0;
 
-//Uso do var_dump com o "!=" e "!==".
+// Criação de variável.
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$SecondValue = 12;
 
-//Criação de variável.
-
-$Valor1 = 12.0;
-
-//Criação de variável.
-
-$Valor2 = 12;
-
-//Impressão de mensagem.
+// Impressão de mensagem.
 
 echo '<strong>Valor e tipo da variável $Valor1 :</strong>'."<br>\n";
 
-//Impressão de "var_dump""
+// Impressão de "var_dump""
 
-var_dump($Valor1);
+var_dump($FirstValue);
 
-//Quebra de linha.
+// Quebra de linha.
 
 echo "<br>\n";
 
-//Impressão de mensagem.
+// Impressão de mensagem.
 
 echo '<strong>Valor e tipo da variável $Valor2 :</strong>'."<br>\n";
 
-//Impressão de "var_dump".
+// Impressão de "var_dump".
 
-var_dump($Valor2);
+var_dump($SecondValue);
 
-//Quebra de linha.
-
-echo "<br>\n";
-
-//Impressão de mensagem.
-
-echo "<strong>Valor 1 é igual á $Valor1.</strong><br>\n";
-
-//Impressão de mensagem.
-
-echo "<strong>Valor 2 é igual á $Valor2.</strong><br>\n";
-
-//Impressão de mensagem.
-
-echo "<strong>$Valor1 e $Valor2 tem o mesmo valor?</strong><br>\n";
-
-//Impressão de "var_dump".
-
-var_dump($Valor1!=$Valor2);
-
-//Quebra de linha.
+// Quebra de linha.
 
 echo "<br>\n";
 
-//Impressão de mensagem e variável.
+// Impressão de mensagem.
 
-echo "<strong>$Valor1 e $Valor2 tem o mesmo valor e tipo?</strong><br>\n";
+echo "<strong>Valor 1 é igual á $FirstValue.</strong><br>\n";
 
-//Impressão de "var_dump".
+// Impressão de mensagem.
 
-var_dump($Valor1!==$Valor2);
+echo "<strong>Valor 2 é igual á $SecondValue.</strong><br>\n";
+
+// Impressão de mensagem.
+
+echo "<strong>$FirstValue e $SecondValue tem o mesmo valor?</strong><br>\n";
+
+// Impressão de "var_dump".
+
+var_dump($FirstValue!=$SecondValue);
+
+// Quebra de linha.
+
+echo "<br>\n";
+
+// Impressão de mensagem e variável.
+
+echo "<strong>$FirstValue e $SecondValue tem o mesmo valor e tipo?</strong><br>\n";
+
+// Impressão de "var_dump".
+
+var_dump($FirstValue!==$SecondValue);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// LEGENDA DE COMANDOS :
+
 // echo = Usado para impressão de mansagens ou valores.
 
-// <strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
 
 // <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas.
 
@@ -85,6 +86,14 @@ var_dump($Valor1!==$Valor2);
 // != = Usado para comparar o valor de duas variáveis e constatar se são diferentes.
 
 // !== = Usado para comparar o valor e tipo de duas variáveis e constatar se são diferentes.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// LEGENDA DE VARIÁVEIS :
+
+// $FirstValue = Recebe um Float, 12.0.
+
+// $SecondValue = Recebe um Int, 12.
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
