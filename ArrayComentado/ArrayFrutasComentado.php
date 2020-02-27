c0157a7bfa7d18ae78f077d255450a8917229a47
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-27 17:08:12 -0300

    Atualização de comentários de arquivo

--- ArrayComentado/ArrayFrutasComentado.php
+++ ArrayComentado/ArrayFrutasComentado.php
@@ -1,39 +1,39 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Criado e atualizado por José Augusto Chaves Izabel
+// Criado e atualizado por José Augusto Chaves Izabel;
 
-// 17/02/2020
+// 17/02/2020;
 
-// Criação de um array do array "Fruit", que salvará nomes de frutas, após criado, dois números serão chamados.
+// Criação de um array do array "Fruit", que salvará nomes de frutas, após criado, dois números serão chamados;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Criação do array "Fruit".
+// Criação do array "Fruit";
 
 $Fruit = array("Maçã","Banana","Jaca","Melão","Abacate","Goiaba","Limão","Laranja","Tangerina","Manga","Uva");
 
-// Impressão de mensagem.
+// Impressão de mensagem;
 
 echo '<strong>Impressão do primeiro número chamado no array "Frutas":</strong>'."<br>\n";
 
-// Impressão do número 1 do array "Fruit".
+// Impressão do número 1 do array "Fruit";
 
 echo $Fruit[1];
 
-// Quebra de linha.
+// Quebra de linha;
 
 echo "<br>\n";
 
-// Impressão de mensagem.
+// Impressão de mensagem;
 
 echo '<strong>Impressão do segundo número chamado no array "Frutas":</strong>'."<br>\n";
 
-// Impressão do número 3 do array "Fruit".
+// Impressão do número 3 do array "Fruit";
 
 echo $Fruit[3];
 
-// Fim do código.
+// Fim do código;
 
 exit;
 
@@ -53,7 +53,7 @@ exit;
 
 // $Exemplo = array(1, 2, 3, 4, 5, "augusto", "teste");
 
-// Array criado.
+// Array criado;
 
 /* Para chamar algum número do array, escreva a a variável em um echo, após a variável coloque o número que deseja chamar entre colchetes, 
 lembrado que o array começa no 0 e não no 1, exemplo:*/
@@ -64,21 +64,21 @@ lembrado que o array começa no 0 e não no 1, exemplo:*/
 
 // LEGENDA DE COMANDOS :
 
-// echo = Imprimir mensagem.
+// echo = Imprimir mensagem;
 
-// <br> = Quebra de linha na exibição final, deve ser usado dentro de aspas, simples ou duplas.
+// <br> = Quebra de linha na exibição final, deve ser usado dentro de aspas, simples ou duplas;
 
-// \n = Quebra de linha no código HTML que será gerado, deve ser usado dentro de aspas, duplas.
+// \n = Quebra de linha no código HTML que será gerado, deve ser usado dentro de aspas, duplas;
 
-// <strong> </strong> = Serve para deixar o texto em negrito, após usar deve fechar o mesmo, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Serve para deixar o texto em negrito, após usar deve fechar o mesmo, deve ser usado dentro de aspas, simples ou duplas;
 
-// exit = Serve para que o código pare de ser interpretado.
+// exit = Serve para que o código pare de ser interpretado;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // LEGENDA DE VARIÁVEIS :
 
-// $Fruit = Array com nomes de frutas.
+// $Fruit = Array com nomes de frutas;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
