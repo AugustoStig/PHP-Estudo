48b17137a1b421106222673dd067773fa076715b
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-27 17:10:19 -0300

    Atualização de comentários

--- ArrayComentado/ArrayCoresNomesComentado.php
+++ ArrayComentado/ArrayCoresNomesComentado.php
@@ -1,67 +1,67 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Criado e atualizado por José Augusto Chaves Izabel
+// Criado e atualizado por José Augusto Chaves Izabel;
 
-// 17/02/2020
+// 17/02/2020;
 
-// Criação de dois arrays, "Name" que salvará nomes de pessoas e "Color"que salvará cores.
+// Criação de dois arrays, "Name" que salvará nomes de pessoas e "Color"que salvará cores;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Criação do array "Color".
+// Criação do array "Color";
 
 $Color = array("Azul","Verde","Vermelho","Amarelo","Roxo","Preto","Cinza","Branco","Laranja","Bege","Lilás","Rosa");
 
-// Criação do array "Name".
+// Criação do array "Name";
 
 $Name = array("Maria","Edna","José","Augusto","Almir","Ana","Luisa","Roberto","Ricardo","Luciana","Natália","João");
 
-// Impressão de mensagem.
+// Impressão de mensagem;
 
 echo '<strong>Impressão do primeiro número chamado no array "Cores":</strong>'."<br>\n";
 
-// Impressão do número 7 do array "Color".
+// Impressão do número 7 do array "Color";
 
 echo $Color[7];
 
-// Quebra de linha.
+// Quebra de linha;
 
 echo "<br>\n";
 
-// Impressão de mensagem.
+// Impressão de mensagem;
 
 echo '<strong>Impressão do primeiro número chamado no array "Nomes":</strong>'."<br>\n";
 
-// Impressão do número 3 do array "Name".
+// Impressão do número 3 do array "Name";
 
 echo $Name[3];
 
-// Quebra de linha.
+// Quebra de linha;
 
 echo "<br>\n";
 
-// Impressão de mensagem.
+// Impressão de mensagem;
 
 echo '<strong>Impressão do segundo número chamado no array "Cores":</strong>'."<br>\n";
 
-// Impressão do número 3 do array "Color".
+// Impressão do número 3 do array "Color";
 
 echo $Color[3];
 
-// Quebra de linha.
+// Quebra de linha;
 
 echo "<br>\n";
 
-// Impressão de mensagem.
+// Impressão de mensagem;
 
 echo '<strong>Impressão do segundo número chamado no array "Nomes":</strong>'."<br>\n";
 
-// Impressão do número 7 do array "Name".
+// Impressão do número 7 do array "Name";
 
 echo $Name[7];
 
-// Fim do código.
+// Fim do código;
 
 exit;
 
@@ -81,7 +81,7 @@ exit;
 
 // $Exemplo = array(1, 2, 3, 4, 5, "augusto", "teste");
 
-// Array criado.
+// Array criado;
 
 /* Para chamar algum número do array, escreva a a variável em um echo, após a variável coloque o número que deseja chamar entre colchetes, 
 lembrado que o array começa no 0 e não no 1, exemplo:*/
@@ -92,23 +92,23 @@ lembrado que o array começa no 0 e não no 1, exemplo:*/
 
 // LEGENDA DE COMANDOS :
 
-// echo = Imprimir mensagem.
+// echo = Imprimir mensagem
 
-// <br> = Quebra de linha na exibição final, deve ser usado dentro de aspas, simples ou duplas.
+// <br> = Quebra de linha na exibição final, deve ser usado dentro de aspas, simples ou duplas;
 
-// \n = Quebra de linha no código HTML que será gerado, deve ser usado dentro de aspas, duplas.
+// \n = Quebra de linha no código HTML que será gerado, deve ser usado dentro de aspas, duplas;
 
-// <strong> </strong> = Serve para deixar o texto em negrito, após usado deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Serve para deixar o texto em negrito, após usado deve ser fechado, deve ser usado dentro de aspas, simples ou duplas;
 
-// exit = Serve para que o código pare de ser interpretado.
+// exit = Serve para que o código pare de ser interpretado;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // LEGENDA DE VARIÁVEIS :
 
-// $Color = Array com cores diversas. 
+// $Color = Array com cores diversas; 
 
-// $Name = Array com nomes diversos.
+// $Name = Array com nomes diversos;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
