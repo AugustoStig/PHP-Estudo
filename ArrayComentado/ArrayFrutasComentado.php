48a2e07311d83832546065c9bad2ab979ecc4e25
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-28 14:40:23 -0300

    Criação de comentários em inglês

--- ArrayComentado/ArrayFrutasComentado.php
+++ ArrayComentado/ArrayFrutasComentado.php
@@ -1,42 +1,52 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Created and updated by José Augusto Chaves Izabel;
 // Criado e atualizado por José Augusto Chaves Izabel;
 
 // 17/02/2020;
 
-// Criação de um array do array "Fruit", que salvará nomes de frutas, após criado, dois números serão chamados;
+// Creation of "Fruit" array, will saves fruit names, after created, two numbers will be called;
+// Criação do array "Fruit", que salvará nomes de frutas, após criado, dois números serão chamados;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Creation of "Fruit" array;
 // Criação do array "Fruit";
 
 $Fruit = array("Maçã","Banana","Jaca","Melão","Abacate","Goiaba","Limão","Laranja","Tangerina","Manga","Uva");
 
+// Message printing;
 // Impressão de mensagem;
 
 echo '<strong>Impressão do primeiro número chamado no array "Fruit":</strong>'."<br>\n";
 
+// Print of number 1 of "Fruit" array;
 // Impressão do número 1 do array "Fruit";
 
 echo $Fruit[1];
 
+// Line break;
 // Quebra de linha;
 
 echo "<br>\n";
 
+// Line break;
 // Quebra de linha;
 
 echo "<br>\n";
 
+// Message print;
 // Impressão de mensagem;
 
 echo '<strong>Impressão do segundo número chamado no array "Fruit":</strong>'."<br>\n";
 
+// Print of number 3 of "Fruit" array;
 // Impressão do número 3 do array "Fruit";
 
 echo $Fruit[3];
 
+// End of code;
 // Fim do código;
 
 exit;
@@ -66,22 +76,44 @@ lembrado que o array começa no 0 e não no 1, exemplo:*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// LEGENDA DE COMANDOS :
+// EXAMPLE :
+// EXEMPLO :
 
-// echo = Imprimir mensagem;
+// To create an array, create a variable first, for example:
+// Para criar um array, deve ser criar a variável primeiro, exemplo:
 
-// <br> = Quebra de linha na exibição final, deve ser usado dentro de aspas, simples ou duplas;
+// $Example = ;
+// $Exemplo = ;
 
-// \n = Quebra de linha no código HTML que será gerado, deve ser usado dentro de aspas, duplas;
+// Then you must inform that that variable will be an array, example:
+// Após deve informar que aquela variável será um array, exemplo:
 
-// <strong> </strong> = Serve para deixar o texto em negrito, após usar deve fechar o mesmo, deve ser usado dentro de aspas, simples ou duplas;
+// $Example = array;
+// $Exemplo = array;
 
-// exit = Serve para que o código pare de ser interpretado;
+// Then open parentheses and put the values ​​that will compose that variable, if it is a string, put it in quotation marks, example:
+// Depois abrir parentesis e colocar os valores que irão compor aquela variável, caso seja string, colocar entre aspas, exemplo:
+
+// $Example = array(1,2,3,4,5,"Augusto","Test");
+// $Exemplo = array(1,2,3,4,5,"Augusto","Teste");
+
+// Created array;
+// Array criado;
+
+/*To call some number in the array, write the variable in an "echo", after the variable put the number you want to call in square brackets,
+remembered that the array starts at number 0 and not at number 1, example:*/
+/* Para chamar algum número do array, escreva a a variável em um "echo", após a variável coloque o número que deseja chamar entre colchetes, 
+lembrado que o array começa no número 0 e não no número 1, exemplo:*/
+
+// echo $Example[3];
+// echo $Exemplo[3];
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// SUBTITLE OF VARIABLES :
 // LEGENDA DE VARIÁVEIS :
 
+// $Fruit = Array with fruit names;
 // $Fruit = Array com nomes de frutas;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
