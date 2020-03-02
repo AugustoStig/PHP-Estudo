de001d93c86d169e7754bf4d5900f56ce2389337
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-03-02 16:56:11 -0300

    Coemntários em inglês

--- DumpComentado/Dump&&Comentado.php
+++ DumpComentado/Dump&&Comentado.php
@@ -1,65 +1,89 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Created and updated by José Augusto Chaves Izabel
 // Criado e atualizado por José Augusto Chaves Izabel
 
 // 12/02/2020
 
-// Utilização do "var_dump", junto com o "&&".
+// Utilization of "var_dump", with a "&&";
+// Utilização do "var_dump", junto com o "&&";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $FirstCalculation = 2 * 3 > 5 && 7 + 2 == 9;
 
-// Impressão de mensagem.
+//
+// Impressão de mensagem;
 
 echo "<strong>(2*3) é maior que 5? E (7+2) é igual á 9?</strong><br>\n";
 
-// Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump($FirstCalculation);
 
-// Quebra de linha.
+// Line break;
+// Quebra de linha;
 
 echo "<br>\n";
 
-// Impressão de mensagem.
+// Line break;
+// Quebra de linha;
+
+echo "<br>\n";
+
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>(2*3) é maior que 5? E (7+2) é igual á 10?</strong><br>\n";
 
-// Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $SecondCalculation = 2 * 3 > 5 && 7 + 2 == 10;
 
-// Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump($SecondCalculation);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// LEGENDAS DE COMANDOS :
+// COMMANDS SUBTITLE :
+// LEGENDA DE COMANDOS :
 
-// echo = Usado para impressão de mansagens ou valores.
+// echo = Used to print messages e variable values;
+// echo = Usado para impressão de mensagens ou valores de variáveis;
 
-// <strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Used to make the text bold, the end of the sentence must be closed, it must be used within quotation marks, single or double;
+// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas;
 
-// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas.
+// <br> = Used to break the line in the final code display, used inside quotes, single or double;
+// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas;
 
-// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas.
+// \n = Used to break the line in the HTML code, generated after, must be used into quotation marks, double;
+// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas;
 
-// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro.
+// var_dump = Used to display the type and value of a variable, after calling it, open parenthesis and put the name of the variable inside;
+// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro;
 
-// && = Usado para a função de "e", quando necessita de que duas ou mais booleanos sejam verdadeiros.
+// && = Used to a function of "and", when you need two or more Booleans to be true;
+// && = Usado para a função de "e", quando necessita de que duas ou mais booleanos sejam verdadeiros;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// LEGENDA DE VARIÁVEIS :
+// VARIABLES SUBTITLE :
+// LEGENDA DE VARIÁVEIS : 
 
-// $FirstCalculation = Recebe um boolean, true ou false.
+// $FirstCalculation = Receive a boolean, true or false
+// $FirstCalculation = Recebe um boolean, true ou false;
 
-// $SecondCalculation = Recebe um boolean, true ou false.
+// $SecondCalculation = Receive a boolean, true or false;
+// $SecondCalculation = Recebe um boolean, true ou false;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
