89eb31a5b5559206e2fe415f158e8b5f7b1bdab4
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-03-02 17:43:19 -0300

    Comentários em inglês

--- DumpComentado/DumpHelloWorldComentado.php
+++ DumpComentado/DumpHelloWorldComentado.php
@@ -1,53 +1,68 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Created and updated by José Augusto Chaves Izabel
 // Criado e atualizado por José Augusto Chaves Izabel
 
 // 11/02/2020
 
-// Código com a intenção de imprimir "Hello World!!!", e após isso, utilizando o "var_dump" mostrar seu tipo e tamanho.
+// Code created with the intention of print a "Hello World!!!", after this, using a "var_dumo" show your type and value;
+// Código com a intenção de imprimir "Hello World!!!", e após isso, utilizando o "var_dump" mostrar seu tipo e tamanho;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo '<strong>Impressão da variável "$Hello" :</strong>'."<br>\n";
 
-// Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $Hello = "Hello World!!!";
 
-// Impressão de variável.
+// Variable print;
+// Impressão de variável;
 
 echo $Hello."<br>\n";
 
-// Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>Especificação da variável 'Hello' utilizado var_dump:</strong><br>\n";
 
-// Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump ($Hello);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// COMMANDS SUBTITLE :
 // LEGENDA DE COMANDOS :
 
-// echo = Usado para impressão de mansagens ou valores.
+// echo = Used to print a message or variable values;
+// echo = Usado para impressão de mansagens ou valores;
 
-// <strong> </strong>= Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> <\strong> = Used to make the text bold, the end of the sentence must be closed, it must be used within quotation marks, single or double;
+// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas;
 
-// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas.
+// <br> = Used to break line in final exibition, must be used into quotation marks, single or double;
+// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas;
 
-// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas.
+// \n = Used for break line in the HTML code, generated after, must be used into quotation marks, double;
+// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas;
 
-// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro.
+// var_dump = 
+// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// VARIABLE SUBTITLE :
 // LEGENDA DE VARIÁVEIS :
 
-// $Hello = Recebe uma string, "Hello World!!!". 
+// $Hello = Receive a string, "Hello World!!!";
+// $Hello = Recebe uma string, "Hello World!!!"; 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
