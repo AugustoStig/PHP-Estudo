1fda3e06284b18805d627f43fa2ed96908433341
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-03-04 17:41:47 -0300

    Atualização de comentários

--- DumpComentado/DumpIntFloatComentado.php
+++ DumpComentado/DumpIntFloatComentado.php
@@ -1,99 +1,127 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Criado e atualizado por José Augusto Chaves Izabel
+// Created and updated by José Augusto Chaves Izabel
+// Criado e atualizado por José Augusto Chaves Izabel
 
-//12/02/2020
+// 12/02/2020
 
-//Uso do "var_dump" em conjunto com o "==" e "===".
+// Use of "var_dump" with a "==" and "===";
+// Uso do "var_dump" em conjunto com o "==" e "===";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $FloatValue = 23.23;
 
-//Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $IntValue = 23;
 
-//Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo '<strong>Valor e tipo da variável $Valor1 :</strong>'."<br>\n";
 
-//Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump($FloatValue);
 
-//Quebra de linha.
+// Line break;
+// Quebra de linha;
 
 echo "<br>\n";
 
-//Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo '<strong>Valor e tipo da variável $Valor2 :</strong>'."<br>\n";
 
-//Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump($IntValue);
 
-//Quebra de linha.
+// Line break;
+// Quebra de linha;
 
 echo "<br>\n";
 
-//Impressão de menssagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>Valor 1 é igual á $FloatValue.</strong><br>\n";
 
-//Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>Valor 2 é igual á $IntValue.</strong><br>\n";
 
-//Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>$FloatValue e $IntValue tem o mesmo valor?</strong><br>\n";
 
-//Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump($FloatValue==$IntValue);
 
-//Quebra de linha.
+// Line break;
+// Quebra de linha;
 
 echo "<br>\n";
 
-//Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>$FloatValue e $IntValue tem o mesmo valor e tipo?</strong><br>\n";
 
-//Impressão de "var_dump".
+// "var_dump" print;
+// Impressão de "var_dump";
 
 var_dump($FloatValue===$IntValue);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// COMMANDS SUBTITLE :
 // LEGENDA DE COMANDOS :
 
-// echo = Usado para impressão de mansagens ou valores.
+// echo = Used to print a message or variable values;
+// echo = Usado para impressão de mensagens ou valores;
 
-// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Used to make the text bold, the end of the sentence must be closed, it must be used within quotation marks, single or double;
+// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas;
 
-// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas.
+// <br> = Used to break line in final exibition of the code, must be used into quotation marks, single or double;
+// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas;
 
-// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas.
+// \n = Used to break line in the HTML code, generated after, must be used into quotation marks, double;
+// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas;
 
-// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro.
+// var_dump = Used to display the type and value of a variable, after calling it, open parenthesis and put the name of the variable inside;
+// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro;
 
-// == = Usado para comparar se duas variáveis tem o mesmo valor.
+// == = Used to compare whether two variables have the same value;
+// == = Usado para comparar se duas variáveis tem o mesmo valor;
 
-// === = Usado para comparar se duas variáveis tem o mesmo valor e tipo.
+// === = Used to compare whether two variables have the same value and type;
+// === = Usado para comparar se duas variáveis tem o mesmo valor e tipo;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// VARIABLE SUBTITLE :
 // LEGENDA DE VARIÁVEIS :
 
-// $IntValue = Recebe um Int, 23.
+// $IntValue = Receive a Int, 23;
+// $IntValue = Recebe um Int, 23;
 
-// $FloatValue = Recebe um Float, 23.23. 
+// $FloatValue = Receive a Float, 23.23;
+// $FloatValue = Recebe um Float, 23.23; 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
