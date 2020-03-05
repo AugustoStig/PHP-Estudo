157765964ff9f12c6039f3c5813acf38b7934565
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-03-05 16:04:11 -0300

    Atualização de comentários em inglês

--- DumpComentado/DumpValorComentado.php
+++ DumpComentado/DumpValorComentado.php
@@ -2,49 +2,72 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Created and updated by José Augusto Chaves Izabel
 // Criado e atualizado por José Augusto Chaves Izabel
 
 // 18/02/2020
 
+// "var_dump" use;
 // Uso do "var_dump";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Variable creation;
 // Criação de variável;
 
 $Value = 199.90;
 
+// Message print;
 // Impressão de mensagem;
 
-echo '<strong>Impressão da variável "$Valor" :</strong>'."<br>\n";
+echo '<strong>Impressão da variável "Value" :</strong>'."<br>\n";
 
+// Variable print;
 // Impressão de variável;
 
 echo $Value;
 
+// Line break;
 // Quebra de linha;
 
 echo "<br>\n";
 
+// Message print;
 // Impressão de mensagem;
 
-echo '<strong>Impressão do "var_dump" da variável "$Valor" :</strong>'."<br>\n";
+echo '<strong>Impressão do "var_dump" da variável "Value" :</strong>'."<br>\n";
 
+// "var_dump" print;
 // Impressão do "var_dump";
 
 var_dump($Value);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// echo = Usado para impressão de mansagens ou valores.
+// COMMANDS SUBTITLE :
+// LEGENDA DE COMANDOS :
 
-// <strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// echo = Use for print messages and variable values;
+// echo = Usado para impressão de mansagens ou valores;
 
-// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Used to make the text bold, the end of the sentence must be closed, it must be used within quotation marks, single or double
+// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas;
 
-// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas.
+// <br> = Used to break line in the final exibition of the code, must be used into quotation marks, single or double;
+// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas;
 
-// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro.
+// \n = Used for break line in the HTML code, generated after, must be used into quotation marks, double;
+// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas;
+
+// var_dump = Used to display the type and value of a variable, after calling it, open parenthesis and put the name of the variable inside;
+// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// COMMANDS SUBTITLE :
+// LEGENDA DE VARIÁVEIS :
+
+// $Value = Receive a float, 199.90;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
