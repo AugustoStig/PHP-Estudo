940da31bf5f6681644dccdc82ff3c0a60b99535c
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-03-05 16:26:36 -0300

    Atualização de arquivos

--- DumpComentado/DumpValorMaiorComentado.php
+++ DumpComentado/DumpValorMaiorComentado.php
@@ -1,63 +1,81 @@
 <?php
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Created and updated by José Augusto Chaves Izabel
 // Criado e atualizado por José Augusto Chaves Izabel
 
 // 18/02/2020
 
-// Uso do "var_dump" com uso de um ">".
+// "var_dump" use, with use of a ">";
+// Uso do "var_dump" com uso de um ">";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $FirstValue = 10;
 
-// Criação de variável.
+// Variable creation;
+// Criação de variável;
 
 $SecondValue = 20;
 
-// Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>Valor 1 é igual á $FirstValue.</strong><br>\n";
 
-// Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>Valor 2 é igual á $SecondValue.</strong><br>\n";
 
-// Quebra de linha.
+// Line break;
+// Quebra de linha;
 
 echo "<br>\n";
 
-// Impressão de mensagem.
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>$FirstValue é maior que $SecondValue?</strong><br>\n";
 
-// Uso do "var_dump".
+// "var_dump" use;
+// Uso do "var_dump";
 
 var_dump($FirstValue>$SecondValue);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// COMMANDS SUBTITLE :
 // LEGENDA DE COMANDOS :
 
-// echo = Usado para impressão de mansagens ou valores.
+// echo = Used to print messages and variable values;
+// echo = Usado para impressão de mensagens ou valores;
 
-// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas.
+// <strong> </strong> = Used to make the text bold, the end of the sentence must be closed, it must be used within quotation marks, single or double;
+// <strong> </strong> = Usado para deixar o texto em negrito, ao final da frase deve ser fechado, deve ser usado dentro de aspas, simples ou duplas;
 
-// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas.
+// <br> = Used to line break in the final exibition of the code, must be used into quotation marks, single or souble;
+// <br> = Usado para quebrar linha na exibição final do código, usado dentro de aspas, simples ou duplas;
 
-// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas.
+// \n = Used to wrap a line in the HTML code that will be generated at the end, it must be used within quotation marks, double;
+// \n = Usado para quebrar linha no código HTML que será gerado ao final, deve ser usado dentro de aspas, duplas;
 
-// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro.
+// var_dump = Used to display the type and value of a variable, after calling it, open parenthesis and put the name of the variable inside;
+// var_dump = Usado para exibir o tipo e valor de uma variável, após chama-lo deve ser abrir parentesis e colocar o nome da variável lá dentro;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// VARIABLE SUBTITLE :
 // LEGENDA DE VARIÁVEIS :
 
-// $FirstValue = Recebe um Int, 10.
+// $FirstValue = Receive a Int, 10;
+// $FirstValue = Recebe um Int, 10;
 
-// $SecondValue = Recebe um Int, 20.
+// $SecondValue = Receive a Int, 20;
+// $SecondValue = Recebe um Int, 20;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
