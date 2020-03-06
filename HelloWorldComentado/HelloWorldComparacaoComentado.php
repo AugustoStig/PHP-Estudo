f74c457e9e7ad4e07399ef751d1774d7da56bf36
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-03-06 16:47:53 -0300

    Atualização de comentários

--- HelloWorldComentado/HelloWorldComparacaoComentado.php
+++ HelloWorldComentado/HelloWorldComparacaoComentado.php
@@ -1,29 +1,60 @@
 <?php
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Criado por José Augusto Chaves Izabel
+// Created and updated by José Augusto Chaves Izabel
+// Criado por José Augusto Chaves Izabel
 
-// 11/02/2020.
+// 11/02/2020
 
-//Código com a intenção de imprimir um "Hello World!!!" 'normal', e um em negrito, com o intuito de fazer uma comparação entre os dois.
+// Code for print a normal "Hello World!!!", and a bold, for comparation;
+// Código para imprimir um "Hello World!!!" normal, e um em negrito, para comparação;
 
-//Mensagem informando o que irá acontecer a seguir, utilizando a tag "<br>" para causar a quebra de linha na exibição final e da "\n" para quebra a linha no HTML.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Message print;
+// Impressão de mensagem;
 
 echo '<strong>Impressão de um "Hello World!!!" normal :</strong>'."<br>\n";
 
-//Mensagem de "Hello World" normal, sem o uso da tag "<strong>".
+// Message print;
+// Impressão de mensagem;
 
 echo "Hello World!!!<br/>\n";
 
-//Quebra de linha
+// Line break;
+// Quebra de linha;
 
 echo "<br>\n";
 
-//Mensagem informando o que irá acontecer a seguir.
+// Message print;
+// Impressão de mensagem;
 
 echo '<strong>Impressão de um "Hello World!!!" negrito :</strong>'."<br>\n";
 
-//Mensagem de "Hello World" em negrito, utilizando a tag "<strong>".
+// Message print;
+// Impressão de mensagem;
 
 echo "<strong>Hello World Strong!!!</strong>"
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// COMMANDS SUBTITLE :
+// LEGENDA DE COMANDOS :
+
+// echo = Print messages or variable values;
+// echo = Imprimir mensagens ou valores de variáveis;
+
+// <strong> </strong> = Deixar o texto em negrito, após seu uso deve ser fechada, deve ser usada entre aspas, simples ou duplas;
+// <strong> </strong> = Make the text bold, after the use must be closed, must be used into quotation marks, single or double;
+
+// <br> = Quebra de linha na exibição final, deve ser usada entre aspas, simples ou duplas;
+// <br> = Line break in the final exibition, must be used into quotation marks, single or double;
+
+// \n = Quebra de linha no código HTML gerado ao final, deve ser usada entre aspas, aspas duplas;
+// \n = Line break in the HTML code, generated before, must be used into quotation marks, double; 
+
+// . = Faz a concatenação entre dois textos, ou duas variáveis, ou ainda, um texto e uma variável;
+// . = Concatenates between two texts, or two variables, or a text and a variable;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
